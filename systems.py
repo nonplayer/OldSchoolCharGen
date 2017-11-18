36a319c4301ff8e12110ac655fa0710f013bc843
N. Phillip Cole <wellthisisbabylon@gmail.com>
2017-11-18 13:45:23 -0800

    placeholders for saves and stat arrays

--- systems.py
+++ systems.py
@@ -7,11 +7,26 @@ TNU = The Nightmares Underground
 DD = Dark Dungeons (my first planned expansion, further down the road)
 '''
 
+statArrays = {
+    'dnd': ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'],
+    'dcc': ['', '', '', '', '', ''],
+    'tnu': ['', '', '', '', '', ''],
+    'pal': ['IQ', 'ME', 'MA', 'PS', 'PP', 'PE', 'PB', 'SPD'],
+}
+
+saves = {
+    'one': [],
+    'three': [],
+    'five': [],
+    'six': [],
+}
+
 systems ={
     'template': {
         'name': 'tmp',
         'fullName': 'Display Name',
         'hasHPs': True,     # BOOL: changes the calculations if the system has hit points
+        'stats': 'tnu',
         'acBase': 10,       # INT: AC base 9 or 10, usually
         'acType': '',       # STR: 'ascend' or 'descend'
         'saves': '',        # STR: 'one', 'three', 'five', or 'six' (such as TBH, 3E, B/X, and 5E, respectively)
@@ -20,6 +35,7 @@ systems ={
         'name': 'tnu',
         'fullName': 'The Nightmares Underneath',
         'hasHPs': False,
+        'stats': 'tnu',
         'acBase': 10,
         'acType': 'ascend',
         'saves': 'six',
@@ -28,6 +44,7 @@ systems ={
         'name': 'dd',
         'fullName': 'Dark Dungeons',
         'hasHPs': True,
+        'stats': 'dnd',
         'acBase': 9,
         'acType': 'descend',
         'saves': 'five',

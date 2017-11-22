3e566368de2b2ecf10e3fd20e2c8012098bd6a3e
N. Phillip Cole <wellthisisbabylon@gmail.com>
2017-11-21 16:43:25 -0800

    completed level 1 PC spells list for blood & treasure

--- spells_osr.py
+++ spells_osr.py
@@ -15,12 +15,38 @@ spells_dd = {
 
 
 spells_bnt = {
-    'cleric': [],
-    'druid': [],
-    'mu': [],
-    'pal': [],
-    'ranger': [],
-    'sorc': [],
+    'bard': [
+        'Alarm', 'Animate Rope', 'Cause Fear', 'Change Self', 'Charm Person', 'Comprehend Languages',
+        'Cure Light Wounds', 'Detect Secret Doors', 'Erase', 'Expeditious Retreat', 'Feather Fall', 'Fool’s Gold',
+        'Foretelling', 'Grease', 'Hideous Laughter', 'Hypnotism', 'Identify', 'Magic Aura', 'Magic Mouth',
+        'Obscure Object', 'Phantasmal Force', 'Remove Fear', 'Sleep', 'Summon Monster I', 'Undetectable Alignment',
+        'Unseen Servant', 'Ventriloquism',
+    ]
+    'cleric': [
+        'Bane', 'Bless', 'Bless Water ', 'Cause Fear', 'Command', 'Comprehend Languages', 'Cure Light Wounds',
+        'Curse Water ', 'Deathwatch', 'Detect Evil', 'Detect Undead', 'Divine Favor', 'Doom', 'Endure Elements',
+        'Entropic Shield', 'Inflict Light Wounds', 'Invisibility to Undead', 'Magic Stone', 'Magic Weapon',
+        'Obscuring Mist', 'Protection from Evil/Good', 'Random Action', 'Remove Fear', 'Sanctuary',
+        'Shield of Faith', 'Summon Monster I',
+    ],
+    'druid': [
+        'Animal Friendship', 'Calm Animals', 'Charm Animal', 'Cure Light Wounds', 'Detect Animals or Plants',
+        'Detect Snares and Pits', 'Elemental Weapon', 'Endure Elements', 'Entangle', 'Faerie Fire', 'Goodberry',
+        'Hide from Animals', 'Jump', 'Longstrider', 'Magic Fang', 'Magic Stone', 'Obscuring Mist',
+        'Pass without Trace', 'Produce Flame', 'Shillelagh', 'Speak with Animals', 'Summon Nature’s Ally I',
+    ],
+    'mu': [
+        'Alarm [A]', 'Animate Rope [T]', 'Burning Hands [EV]', 'Cause Fear [N]', 'Change Self [I]',
+        'Charm Person [EN]', 'Chill Touch [N]', 'Color Spray [I]', 'Comprehend Languages [D]',
+        'Detect Secret Doors [D]', 'Detect Undead [D]', 'Elemental Weapon [T]', 'Endure Elements [A]',
+        'Energy Missile [EV]', 'Enlarge Person [T]', 'Erase [T]', 'Expeditious Retreat [T]', 'Feather Fall [T]',
+        'Floating Disk [EV]', 'Fool’s Gold [T]', 'Grease [C]', 'Hold Portal [A]', 'Hypnotism [EN]', 'Identify [D]',
+        'Ill Omen [EN]', 'Jump [T]', 'Mage Armor [C]', 'Magic Aura [I]', 'Magic Missile [EV]', 'Magic Weapon [T]',
+        'Mind Thrust [EV]', 'Mount [C]', 'Obscuring Mist [C]', 'Phantasmal Force [I]', 'Precognition [D]',
+        'Protection from Evil [A]', 'Protection from Good [A]', 'Ray of Enfeeblement [N]', 'Reduce Person [T]',
+        'Shield [A]', 'Shocking Grasp [EV]', 'Sleep [EN]', 'Summon Monster I [C]', 'True Strike [D]',
+        'Unseen Servant [C]', 'Ventriloquism [I]',
+    ],
 }
 
 
@@ -29,10 +55,19 @@ cantrips_bnt = {
         'Audible Glamer', 'Dancing Lights', 'Daze', 'Detect Magic', 'Flare', 'Know Direction', 'Light', 'Lullaby',
         'Mage Hand', 'Mending', 'Message', 'Open/Close', 'Prestidigitation', 'Resistance', 'Summon Instrument',
     ],
-    'cleric': [],
-    'druid': [],
-    'mu': [],
-    'sorc': [],
+    'cleric': [
+        'Create Water', 'Cure Minor Wounds', 'Detect Magic', 'Detect Poison', 'Guidance', 'Inflict Minor Wounds',
+        'Light', 'Mending', 'Purify Food and Drink', 'Read Magic', 'Resistance', 'Virtue',
+    ],
+    'druid': [
+        'Create Water', 'Cure Minor Wounds', 'Detect Magic', 'Detect Poison', 'Discern Aura', 'Flare', 'Guidance',
+        'Know Direction', 'Light', 'Mending', 'Purify Food and Drink', 'Read Magic', 'Resistance', 'Virtue',
+    ],
+    'mu': [
+        'Acid Splash', 'Audible Glamer', 'Dancing Lights', 'Daze', 'Detect Magic', 'Detect Poison', 'Disrupt Undead',
+        'Flare', 'Light', 'Mage Hand', 'Mending', 'Message', 'Open/Close', 'Prestidigitation', 'Ray of Frost',
+        'Read Magic', 'Resistance', 'Smoke Image', 'Touch of Fatigue', 'Wizard Mark',
+    ],
 }
 
 
